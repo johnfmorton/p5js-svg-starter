b96321d242a25b74b064f1ddfd0227707b8ff4aa
John F Morton <morton@jmx2.com>
2024-05-27 14:42:25 -0400

    Added lil-gui

--- main.ts
+++ main.ts
@@ -4,37 +4,99 @@ import './src/css/input.css'
 import p5 from 'p5'
 import init, { p5SVG } from 'p5.js-svg'
 
+import GUI from 'lil-gui'
+
 import { createQtGrid, randomBias, randomSnap, random } from '@georgedoescode/generative-utils'
 
 init(p5)
 
-let pInstance;
+let pInstance
+
+const gui = new GUI().title('Sketch controls')
+const obj = {
+  title: 'Pen Plotter Experiment', // string
+  num: 10, // number
+  bool: true, // boolean
+  color: '#ff0000', // color
+  range: 50, // range
+  text: 'Hello', // text
+  select: ['option1', 'option2', 'option3'], // select
+  radio: 'option1', // radio
+  drawRectLines: false , // checkbox
+  button: () => alert('Hello'), // button,
+  redraw: () => pInstance.redraw(),
+  saveSvg: () => pInstance.save('sketch.svg'),
+  resize: () => {pInstance.reset()},
+  width: 1056,
+  height: 816,
+  lineSpacingForCircles: 3,
+  lineSpacingForSquares: 3,
+  centerTheRect: true
+}
+
+// gui.add(document, 'title')
+
+
+gui.add(obj, 'num', 0, 100).step(1)
+gui.add(obj, 'bool')
+gui.add(obj, 'color')
+gui.add(obj, 'range', 0, 100)
+gui.add(obj, 'text')
+gui.add(obj, 'select')
+gui.add(obj, 'radio', ['option1', 'option2', 'option3'])
+gui.add(obj, 'lineSpacingForCircles', 1, 10).name('Circles line spacing').step(1)
+gui.add(obj, 'lineSpacingForSquares', 1, 10).name('Squares line spacing').step(1)
+gui.add(obj, 'drawRectLines').name('Draw rect lines?')
+gui.add(obj, 'centerTheRect').name('Center the rect?')
+gui.add(obj, 'saveSvg').name('Save SVG')
+gui.add(obj, 'redraw').name('Redraw')
+const canvasOption = gui.addFolder('Canvas options')
+canvasOption.close()
+canvasOption.add(obj, 'title')
+canvasOption.add(obj, 'width')
+canvasOption.add(obj, 'height')
+canvasOption.add(obj, 'resize').name('Reset Canvas')
+
 
 // p5 sketch
 const sketch = (p: p5SVG) => {
+
+
   // Assign the sketch instance to the global variable
   pInstance = p
 
   // SVG is sized to be a full 8 1/2 x 11 inch document when opened in InkScape
-  const width = 1056
-  const height = 816
+  // const width = obj.width
+  // const height = obj.height
+
+  const getWidth = () => obj.width;
+  const getHeight = () => obj.height;
 
   // const colors: string[] = ['#7257fa', '#ffd53d', '#1D1934', '#F25C54']
   const colors: string[] = ['#00C5F0', '#96F000', '#BC00F0', '#F07000', '#84349B', '#386670']
   type Color = (typeof colors)[number]
 
+  setPageTitle(obj.title)
+
   p.setup = () => {
     // Setup the canvas
-    p.createCanvas(width, height, p.SVG)
+    p.createCanvas(getWidth(), getHeight(), p.SVG)
     // p.createCanvas(width, height)
 
     // Don't loop the draw function
     p.noLoop()
   }
 
+  // reset
+  p.reset = () => {
+    setPageTitle(obj.title)
+    p.resizeCanvas(getWidth(), getHeight())
+    // p.setup()
+  }
+
   p.draw = () => {
     // clear the background
-    p.clear();
+    p.clear()
 
     // Set background color
     // p.background(256)
@@ -43,20 +105,23 @@ const sketch = (p: p5SVG) => {
     p.noFill()
 
     const focus = {
-      x: random(0, width),
-      y: random(0, height)
+      x: random(0, getWidth()),
+      y: random(0, getHeight())
     }
 
     const points = [...Array(100)].map(() => {
       return {
-        x: randomBias(0, width, focus.x, 1),
-        y: randomBias(0, height, focus.y, 1),
+        x: randomBias(0, getWidth(), focus.x, 1),
+        y: randomBias(0, getHeight(), focus.y, 1),
         width: 2,
         height: 2
       }
     })
 
-    const grid = createQtGrid({ width, height, points, gap: 0, maxQtObjects: 3, maxQtLevels: 100 })
+const width = obj.width
+const height = obj.height
+
+    let grid = createQtGrid({ width, height, points, gap: 0, maxQtObjects: 3, maxQtLevels: 100 })
 
     // loop through the grid.areas
 
@@ -70,7 +135,9 @@ const sketch = (p: p5SVG) => {
       // draw the rect for the background
       p.rectMode(p.CORNER)
       // draw a rectabgle for the area. You may want to turn this off
-      // p.rect(area.x, area.y, area.width, area.height)
+      if (obj.drawRectLines) {
+        p.rect(area.x, area.y, area.width, area.height)
+      }
 
       const centerX = area.x + area.width / 2
       const centerY = area.y + area.height / 2
@@ -115,7 +182,7 @@ const sketch = (p: p5SVG) => {
       // fillCircleConcentric(0, 0, minDimension / dimensionOffset, 20, true)
       p.rotate(angle1)
       if (randomNum < 0.95) {
-        jankyFillEllipse(0, 0, minDimension / dimensionOffset - 5, minDimension / dimensionOffset - 5, 3)
+        jankyFillEllipse(0, 0, minDimension / dimensionOffset - 5, minDimension / dimensionOffset - 5, obj.lineSpacingForCircles)
       }
       // pick a new fill color
       let color3: Color
@@ -138,8 +205,7 @@ const sketch = (p: p5SVG) => {
         // in the center of the rect, draw a circle
         // p.rect(0, 0, minDimension / 3, minDimension / 3)
       } else {
-
-        jankyFilledRect(0, 0, minDimension / 3, minDimension / 3, 3, true)
+        jankyFilledRect(0, 0, minDimension / 3, minDimension / 3, obj.lineSpacingForSquares, obj.centerTheRect)
       }
       // Pop matrix to restore previous transformation state
       p.pop()
@@ -151,15 +217,6 @@ const sketch = (p: p5SVG) => {
     console.log('grid', grid.areas)
   }
 }
-
-document.getElementById('save-button')?.addEventListener('click', () => {
-  const timestamp = new Date().getTime()
-  pInstance.save(`sketch-${timestamp}.svg`)
-})
-
-document.getElementById('regenerate-button')?.addEventListener('click', () => {
-  pInstance.redraw()
-})
 
 
 // fill rect
@@ -175,16 +232,14 @@ function filledRect(x, y, w, h, lineSpacing, centerFill = true) {
   pInstance.push() // Save the current drawing settings
 
   for (let i = startY; i < y + h; i += lineSpacing) {
-
     if (centerFill) {
-        pInstance.line(x - w / 2, i - w / 2, x + w - w / 2, i - w / 2)
-      } else {
-        pInstance.line(x, i, x + w, i)
-      }
+      pInstance.line(x - w / 2, i - w / 2, x + w - w / 2, i - w / 2)
+    } else {
+      pInstance.line(x, i, x + w, i)
+    }
   }
   pInstance.pop() // Restore the drawing settings
 }
-
 
 // fill rect
 function jankyFilledRect(x, y, w, h, lineSpacing, centerFill = true) {
@@ -199,19 +254,17 @@ function jankyFilledRect(x, y, w, h, lineSpacing, centerFill = true) {
   pInstance.push() // Save the current drawing settings
 
   for (let i = startY; i < y + h; i += lineSpacing) {
-
     let jankyI = i + randomSnap(-1, 1, 0.5)
     let jankyEnd = i + randomSnap(-1, 1, 0.5)
 
     if (centerFill) {
-        pInstance.line(x - w / 2, jankyI - w / 2, x + w - w / 2, jankyEnd - w / 2)
-      } else {
-        pInstance.line(x, jankyI, x + w, jankyI)
-      }
+      pInstance.line(x - w / 2, jankyI - w / 2, x + w - w / 2, jankyEnd - w / 2)
+    } else {
+      pInstance.line(x, jankyI, x + w, jankyI)
+    }
   }
   pInstance.pop() // Restore the drawing settings
 }
-
 
 function fillCircleConcentric(cx, cy, radius, lineSpacing, centerFill = true) {
   // Draw the boundary of the circle
@@ -233,56 +286,57 @@ function fillCircleConcentric(cx, cy, radius, lineSpacing, centerFill = true) {
 // fillCircle function using radial lines
 function fillCircleRadial(cx, cy, radius, lineSpacing, centerFill = true) {
   // Draw the boundary of the circle
-  pInstance.stroke(0); // Set stroke color
-  pInstance.noFill(); // No fill for the boundary
-  pInstance.ellipse(cx, cy, 2 * radius, 2 * radius); // Draw the outer circle
+  pInstance.stroke(0) // Set stroke color
+  pInstance.noFill() // No fill for the boundary
+  pInstance.ellipse(cx, cy, 2 * radius, 2 * radius) // Draw the outer circle
 
   // Start drawing radial lines within the circle
-  pInstance.push(); // Save the current drawing settings
+  pInstance.push() // Save the current drawing settings
 
   // Calculate the number of lines based on the circumference and line spacing
-  const numLines = Math.floor(2 * Math.PI * radius / lineSpacing);
-  const angleStep = 2 * Math.PI / numLines; // Angle between each line
+  const numLines = Math.floor((2 * Math.PI * radius) / lineSpacing)
+  const angleStep = (2 * Math.PI) / numLines // Angle between each line
 
   for (let i = 0; i < numLines; i++) {
-    let angle = i * angleStep;
-    let x = cx + radius * Math.cos(angle);
-    let y = cy + radius * Math.sin(angle);
-    pInstance.line(cx, cy, x, y); // Draw line from center to the edge
+    let angle = i * angleStep
+    let x = cx + radius * Math.cos(angle)
+    let y = cy + radius * Math.sin(angle)
+    pInstance.line(cx, cy, x, y) // Draw line from center to the edge
   }
 
-  pInstance.pop(); // Restore the drawing settings
+  pInstance.pop() // Restore the drawing settings
 }
 
 // fillEllipse function using horizontal lines
 function fillEllipse(cx, cy, width, height, lineSpacing) {
   // Draw the boundary of the ellipse
-  pInstance.stroke(0); // Set stroke color
-  pInstance.noFill(); // No fill for the boundary
+  pInstance.stroke(0) // Set stroke color
+  pInstance.noFill() // No fill for the boundary
   // pInstance.ellipse(cx, cy, width, height); // Draw the outer ellipse
 
   // Start drawing horizontal lines within the ellipse
-  pInstance.push(); // Save the current drawing settings
+  pInstance.push() // Save the current drawing settings
 
   // Calculate y coordinates where lines will be drawn
-  let startY = cy - height / 2;
-  let endY = cy + height / 2;
+  let startY = cy - height / 2
+  let endY = cy + height / 2
 
   for (let y = startY; y <= endY; y += lineSpacing) {
     // Calculate the x coordinates of the intersection of the line with the ellipse
-    let deltaY = cy - y; // Vertical distance from the center
-    let part = deltaY / (height / 2); // Normalized vertical distance
-    let xLength = (width / 2) * Math.sqrt(1 - part * part); // Horizontal distance from the center
+    let deltaY = cy - y // Vertical distance from the center
+    let part = deltaY / (height / 2) // Normalized vertical distance
+    let xLength = (width / 2) * Math.sqrt(1 - part * part) // Horizontal distance from the center
 
-    let startX = cx - xLength;
-    let endX = cx + xLength;
+    let startX = cx - xLength
+    let endX = cx + xLength
 
-    if (xLength > 0) { // Check if the calculated width is positive
-      pInstance.line(startX, y, endX, y); // Draw the horizontal line within the ellipse
+    if (xLength > 0) {
+      // Check if the calculated width is positive
+      pInstance.line(startX, y, endX, y) // Draw the horizontal line within the ellipse
     }
   }
 
-  pInstance.pop(); // Restore the drawing settings
+  pInstance.pop() // Restore the drawing settings
 }
 
 // fillEllipse function using horizontal lines
@@ -300,7 +354,6 @@ function jankyFillEllipse(cx, cy, width, height, lineSpacing) {
   let endY = cy + height / 2
 
   for (let y = startY; y <= endY; y += lineSpacing) {
-
     // Calculate the x coordinates of the intersection of the line with the ellipse
     let deltaY = cy - y // Vertical distance from the center
     let part = deltaY / (height / 2) // Normalized vertical distance
@@ -321,6 +374,15 @@ function jankyFillEllipse(cx, cy, width, height, lineSpacing) {
   }
 
   pInstance.pop() // Restore the drawing settings
+}
+
+function setPageTitle(title: string) {
+  document.title = title
+  // find the h1 element and set the text
+  const h1 = document.querySelector('h1')
+  if (h1) {
+    h1.textContent = title
+  }
 }
 
 // Create a new p5 instance and attach the sketch
